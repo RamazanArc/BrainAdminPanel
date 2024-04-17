b959a0396b696815f5e6eafd2d5e9fd48ae81885
Ramazan Arıcı <ramazanarc5@gmail.com>
2024-04-17 16:39:13 +0300

    Model and Service Updated

--- app/Models/Login.ts
+++ app/Models/Login.ts
@@ -10,4 +10,16 @@ export default class Login {
   getMobileAuthCode(body: any) {
     return API.post("/Login/GetMobileAuthCode", body);
   }
+  getProfileWithToken(body: any) {
+    return API.post("/Login/GetProfileWithToken", body);
+  }
+  setNotificationToken(body: any) {
+    return API.post("/Login/SetNotificationToken", body);
+  }
+  checkToken(body: any) {
+    return API.get("/Login/CheckToken", body);
+  }
+  checkScreen(body: any) {
+    return API.get("/Login/checkScreen", body);
+  }
 }
--- app/api/service.ts
+++ app/api/service.ts
@@ -2,7 +2,6 @@ import axios, { AxiosInstance } from "axios";
 
 const client: AxiosInstance = axios.create({
   baseURL: "https://brain2022.brmagazacilik.com.tr:2023/api",
-  // "https://brain2022.brmagazacilik.com.tr:2023/swagger/api"
 });
 
 export const API = {
@@ -16,19 +15,7 @@ export const API = {
       return [null, error] as any;
     }
   },
-  // post: async (url: string, data?: any, header?: any, responsee?: any) => {
-  //   try {
-  //     await axios.post(
-  //       "https://brain2022.brmagazacilik.com.tr:2023/api/Login/GetMobileAuthCode?id=ramazan&mobilephone=5452642588",
-  //       {}
-  //     );
-  //     const response = await client.post(url, data, header);
-  //     console.log(data);
-  //     return [response, null] as any;
-  //   } catch (error) {
-  //     return [null, error] as any;
-  //   }
-  // },
+
   post: async (url: string, data?: any) => {
     try {
       const response = await client.post(
@@ -43,4 +30,18 @@ export const API = {
       return [null, error] as any;
     }
   },
+
+  // post: async (url: string, data?: any, header?: any, responsee?: any) => {
+  //   try {
+  //     await axios.post(
+  //       "https://brain2022.brmagazacilik.com.tr:2023/api/Login/GetMobileAuthCode?id=ramazan&mobilephone=5452642588",
+  //       {}
+  //     );
+  //     const response = await client.post(url, data, header);
+  //     console.log(data);
+  //     return [response, null] as any;
+  //   } catch (error) {
+  //     return [null, error] as any;
+  //   }
+  // },
 };
